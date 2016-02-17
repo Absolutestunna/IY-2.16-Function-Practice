7a7879ee904128efbc1b63016887df72350ee26a
Joshua Abu <joshujah007@gmail.com>
2016-02-17 08:57:24 -0500

    update

--- exercises.js
+++ exercises.js
@@ -166,8 +166,10 @@ function filterLongWords(words, i){
 // ---------------------
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
-
+charFreq("abbabcbdbabdbdbabababcbcbab");
 function charFreq(string){
+  var strSplit = string.split('');
+  
     "use strict";
     //...
 }
